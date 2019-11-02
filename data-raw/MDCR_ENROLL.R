8b53bd9958dbe5ec7c8a0c9f1a518a4ab1c8eb91
Peter DeWitt (BigBlue) <peter.dewitt@cuanschutz.edu>
2019-11-01 22:41:52 -0600

    add code for building enrollment ab 08

--- data-raw/MDCR_ENROLL.R
+++ data-raw/MDCR_ENROLL.R
@@ -314,4 +314,49 @@ MDCR_ENROLL_AB_07 %<>% as.data.frame
 save(MDCR_ENROLL_AB_07, file = "../data/MDCR_ENROLL_AB_07.rda")
 # }}}
 
+MDCR_ENROLL_AB_08 <- #{{{
+  lapply(
+         c(paste0(tmpdir, "/2013/CPS_MDCR_ENROLL_AB_8.xlsx"),
+           paste0(tmpdir, "/2014/CPS_MDCR_ENROLL_AB_8.xlsx"),
+           paste0(tmpdir, "/2015/CPS_MDCR_ENROLL_AB_8.xlsx"),
+           paste0(tmpdir, "/2016/CPS_MDCR_ENROLL_AB_8.xlsx"),
+           paste0(tmpdir, "/2017/CPS_MDCR_ENROLL_AB_8.xlsx"))
+         ,
+         readxl::read_excel,
+         skip = 3,
+         col_type = "text"
+  )
+
+MDCR_ENROLL_AB_08[[1]]$Year <- 2013L
+MDCR_ENROLL_AB_08[[2]]$Year <- 2014L
+MDCR_ENROLL_AB_08[[3]]$Year <- 2015L
+MDCR_ENROLL_AB_08[[4]]$Year <- 2016L
+MDCR_ENROLL_AB_08[[5]]$Year <- 2017L
+
+MDCR_ENROLL_AB_08 %<>%
+  dplyr::bind_rows(.) %>%
+  dplyr::distinct(.) %>%
+  dplyr::filter(!is.na(`Total Medicare Enrollees`)) %>%
+  dplyr::mutate_at(dplyr::vars(-`Area of Residence`, -Year),
+                   ~ suppressWarnings(as.numeric(.)))
+
+
+cat("# Auto Generated. Do not edit by hand",
+    "#' Total Medicare Enrollment:  Part A and/or Part B Enrollees, by Type of Entitlement and Area of Residence.",
+    "#'",
+    "#' Total Medicare Enrollment:  Part A and/or Part B Enrollees, by Type of Entitlement and Area of Residence.",
+    "#'",
+    "#' NOTES:  The enrollment counts are determined using a person-year methodology.  Numbers and percentages may not add to totals because of rounding.",
+    "#'",
+    "#' There are some NA values in this data set.  These NA values are associated with counts between 1 and 10 have been suppressed because of CMS rules to protect the privacy of beneficiaries, or have been cross-suppressed to prevent the recalculation of suppressed counts between 1 and 10.",
+    "#'",
+    "#' @source Centers for Medicare & Medicaid Services, Office of Enterprise Data and Analytics, CMS Chronic Conditions Data Warehouse.",
+    "#'",
+    "\"MDCR_ENROLL_AB_08\"",
+    sep = "\n",
+    file = "../R/MDCR_ENROLL_AB_08.R")
+
+MDCR_ENROLL_AB_08 %<>% as.data.frame
+save(MDCR_ENROLL_AB_08, file = "../data/MDCR_ENROLL_AB_08.rda")
+# }}}
 
