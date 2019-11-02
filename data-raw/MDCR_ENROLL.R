25d0923fe20ca2592ba14bc899b1c92436da45f4
Peter DeWitt (BigBlue) <peter.dewitt@cuanschutz.edu>
2019-11-01 22:26:03 -0600

    add code for building enroll ab 07

--- data-raw/MDCR_ENROLL.R
+++ data-raw/MDCR_ENROLL.R
@@ -270,4 +270,48 @@ MDCR_ENROLL_AB_06 %<>% as.data.frame
 save(MDCR_ENROLL_AB_06, file = "../data/MDCR_ENROLL_AB_06.rda")
 # }}}
 
+MDCR_ENROLL_AB_07 <- #{{{
+  lapply(
+         c(paste0(tmpdir, "/2013/CPS_MDCR_ENROLL_AB_7.xlsx"),
+           paste0(tmpdir, "/2014/CPS_MDCR_ENROLL_AB_7.xlsx"),
+           paste0(tmpdir, "/2015/CPS_MDCR_ENROLL_AB_7.xlsx"),
+           paste0(tmpdir, "/2016/CPS_MDCR_ENROLL_AB_7.xlsx"),
+           paste0(tmpdir, "/2017/CPS_MDCR_ENROLL_AB_7.xlsx"))
+         ,
+         readxl::read_excel,
+         skip = 3,
+         col_type = "text"
+  )
+
+MDCR_ENROLL_AB_07[[1]]$Year <- 2013L
+MDCR_ENROLL_AB_07[[2]]$Year <- 2014L
+MDCR_ENROLL_AB_07[[3]]$Year <- 2015L
+MDCR_ENROLL_AB_07[[4]]$Year <- 2016L
+MDCR_ENROLL_AB_07[[5]]$Year <- 2017L
+
+MDCR_ENROLL_AB_07 %<>%
+  dplyr::bind_rows(.) %>%
+  dplyr::distinct(.) %>%
+  dplyr::filter(!is.na(`Part A and/or Part B Total`)) %>%
+  dplyr::mutate_at(dplyr::vars(-`Area of Residence`, -Year),
+                   as.numeric)
+
+
+cat("# Auto Generated. Do not edit by hand",
+    "#' Total Medicare Enrollment:  Part A and/or Part B Total, Aged, and Disabled Enrollees, by Area of Residence",
+    "#'",
+    "#' Total Medicare Enrollment:  Part A and/or Part B Total, Aged, and Disabled Enrollees, by Area of Residence",
+    "#'",
+    "#' NOTES:  The enrollment counts are determined using a person-year methodology.  Numbers and percentages may not add to totals because of rounding.",
+    "#'",
+    "#' @source Centers for Medicare & Medicaid Services, Office of Enterprise Data and Analytics, CMS Chronic Conditions Data Warehouse.",
+    "#'",
+    "\"MDCR_ENROLL_AB_07\"",
+    sep = "\n",
+    file = "../R/MDCR_ENROLL_AB_07.R")
+
+MDCR_ENROLL_AB_07 %<>% as.data.frame
+save(MDCR_ENROLL_AB_07, file = "../data/MDCR_ENROLL_AB_07.rda")
+# }}}
+
 
